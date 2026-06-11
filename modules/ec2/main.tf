dff2b390cf2764f65f7729dd9794e8ca85e6a13a
asavari-parit <asavari.parit@operisoft.com>
2026-06-11 11:49:43 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,9 +1,35 @@
-module "ec2" {
-  source = "./modules/ec2"
+# Security Group
 
-  ami_id        = "ami-xxxxxxxx"
-  instance_type = "t2.micro"
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2-security-group"
+  description = "Allow SSH"
+  vpc_id      = var.vpc_id
 
-  subnet_id = module.vpc.subnet_id
-  vpc_id    = module.vpc.vpc_id
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance
+
+resource "aws_instance" "ec2" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  tags = {
+    Name = "terraform-ec2"
+  }
 }
