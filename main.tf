3392e3855b282dad965b073ddae77f0b79e636ee
asavari-parit <asavari.parit@operisoft.com>
2026-06-11 11:54:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,29 @@
+provider "aws" {
+  region = "ap-south-1"
+}
 
+module "vpc" {
+  source = "./modules/vpc"
+
+  vpc_cidr    = "10.0.0.0/16"
+  subnet_cidr = "10.0.1.0/24"
+
+  vpc_name    = "learning-vpc"
+  subnet_name = "public-subnet"
+}
+
+module "ec2" {
+  source = "./modules/ec2"
+
+  ami_id        = "YOUR_AMI_ID"
+  instance_type = "t2.micro"
+
+  subnet_id = module.vpc.subnet_id
+  vpc_id    = module.vpc.vpc_id
+}
+
+module "s3" {
+  source = "./modules/s3"
+
+  bucket_name = "your-unique-bucket-name"
+}
