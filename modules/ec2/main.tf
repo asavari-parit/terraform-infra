4f659c6bbcbef0440f7423e9e80ccc02847c850e
asavari-parit <asavari.parit@operisoft.com>
2026-06-11 11:48:21 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,1 +1,9 @@
+module "ec2" {
+  source = "./modules/ec2"
 
+  ami_id        = "ami-xxxxxxxx"
+  instance_type = "t2.micro"
+
+  subnet_id = module.vpc.subnet_id
+  vpc_id    = module.vpc.vpc_id
+}
