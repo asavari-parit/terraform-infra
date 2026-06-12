7a47c2c85530982a89caa5ece46df8e96afea7d6
asavari-parit <asavari.parit@operisoft.com>
2026-06-12 11:58:24 +0530

    Configure Terraform backend with S3 and DynamoDB

--- backend.tf
+++ backend.tf
@@ -1,1 +1,9 @@
-
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-732203471303"
+    key            = "dev/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
