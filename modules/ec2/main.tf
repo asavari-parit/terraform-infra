5a4251041bfb103a0929d3020ec6c5100afa01a8
asavari-parit <asavari.parit@operisoft.com>
2026-06-12 11:16:51 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,7 +1,7 @@
 # Security Group
 resource "aws_security_group" "ec2_sg" {
   name        = "ec2-security-group"
-  description = "Allow SSH"
+  description = "Allow SSH and HTTP"
   vpc_id      = var.vpc_id
 
   ingress {
@@ -12,21 +12,41 @@ resource "aws_security_group" "ec2_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "ec2-security-group"
+  }
 }
 
 # EC2 Instance
 resource "aws_instance" "ec2" {
-  ami           = var.ami_id
-  instance_type = "t3.small"
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
+  associate_public_ip_address = true
 
-  subnet_id              = var.subnet_id
-  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y nginx
+              systemctl enable nginx
+              systemctl start nginx
+              EOF
 
   tags = {
     Name = "terraform-ec2"
